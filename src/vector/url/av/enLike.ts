de84ff635271414bb54d27d42826cf3e80e9eeb0
853 <sonic853@qq.com>
2022-05-26 16:21:19 +0800

    增加JS方面的三连实现方式： MotooriKashin/Bilibili-Old#307 缺少CSS 缺少要加入的svg圆环

--- src/vector/url/av/enLike.ts
+++ src/vector/url/av/enLike.ts
@@ -2,11 +2,18 @@ namespace API {
     export class enLike {
         /** aid备份 */
         aid: number = <any>undefined;
-        /** 锚节点 */
+        /** 锚节点 硬币 */
         coin: HTMLElement = <any>undefined;
+        /** 锚节点 收藏 */
+        fav: HTMLElement = <any>undefined;
+        /** 锚节点 点赞 */
         span: HTMLSpanElement = <any>undefined;
         /** 点赞了吗？ */
         liked = false;
+        /** 是否按住点赞 */
+        hold = false;
+        /** 按住时的 setTimeout */
+        holdTimeout: number = <any>undefined;
         /** 点赞数 */
         number = 0;
         /** 页面类型 */
@@ -20,7 +27,8 @@ namespace API {
             this.number = num;
             doWhile(() => { // 目标节点存在后才初始化
                 this.coin = type === "watchlater" ? document.querySelector<any>(".u.coin") : document.querySelector<any>("[report-id*=coin]");
-                return this.coin && aid;
+                this.fav = type === "watchlater" ? document.querySelector<any>(".u.fav") : document.querySelector<any>("[report-id*=collect]");
+                return this.coin && this.fav && aid;
             }, () => this.init())
         }
         init() {
@@ -30,7 +38,9 @@ namespace API {
             this.span.classList.add("ulike");
             (<any>this).coin.parentElement.insertBefore(this.span, this.coin);
             this.changeLiked();
-            this.span.addEventListener("click", () => this.setLike());
+            this.span.addEventListener("click", () => this.setLike()); // 单击点赞
+            this.span.addEventListener("mousedown", ev => this.holdLike(ev)); // 按住触发
+            this.span.addEventListener("mousemove", ev => this.unHoldLike()); // 按住时移动了会触发，在点赞处经过也会触发
             switchVideo(() => this.switch());
             try {
                 !this.number && xhr({ // 获取点赞数
@@ -68,6 +78,10 @@ namespace API {
         }
         /** 点赞响应 */
         setLike() {
+            if (this.hold) {
+                this.unHoldLike();
+                return
+            }
             if (uid) {
                 const like = this.liked ? 2 : 1;
                 xhr({
@@ -86,6 +100,40 @@ namespace API {
                 toast.warning("请先登录 щ(ʘ╻ʘ)щ");
                 biliQuickLogin();
             }
+        }
+        /** 按住点赞 */
+        holdLike(ev: MouseEvent) {
+            if (!this.hold && ev.button == 0) { // 之前是否按住过，是否为左键
+                this.holdTimeout = setTimeout(() => {
+                    this.hold = true; // 判定为按住
+                    if (this.hold) {
+                        this.span.classList.add("shake");
+                        this.coin.classList.add("circle");
+                        this.fav.classList.add("circle");
+                        this.holdTimeout = setTimeout(() => {
+                            if (this.hold) {
+                                // Todo: 触发三连
+
+                                // Todo结束
+                                this.unHoldLike();
+                            }
+                        }, 2500);
+                    }
+                }, 800);
+            }
+        }
+        /** 取消按住 */
+        unHoldLike() {
+            if (this.hold) {
+                this.hold = false;
+            }
+            if (this.holdTimeout) {
+                clearTimeout(this.holdTimeout);
+                this.holdTimeout = undefined;
+            }
+            this.span.classList.remove("shake");
+            this.coin.classList.remove("circle");
+            this.fav.classList.remove("circle");
         }
         /** 图标及数目变化 */
         changeLiked() {
