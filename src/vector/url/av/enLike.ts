0d97de5853bcdcca83fbe72a22d98700c309c480
853 <sonic853@qq.com>
2022-05-26 17:35:42 +0800

    修补各个页面类型下的三连

--- src/vector/url/av/enLike.ts
+++ src/vector/url/av/enLike.ts
@@ -26,9 +26,22 @@ namespace API {
             this.type = type;
             this.number = num;
             doWhile(() => { // 目标节点存在后才初始化
-                this.coin = type === "watchlater" ? document.querySelector<any>(".u.coin") : document.querySelector<any>("[report-id*=coin]");
-                this.fav = type === "watchlater" ? document.querySelector<any>(".u.fav") : document.querySelector<any>("[report-id*=collect]");
-                return this.coin && aid;
+                switch (this.type) {
+                    case "bangumi": {
+                        this.coin = document.querySelector<any>("[report-id*=coin]");
+                        return this.coin && aid;
+                    }
+                    case "watchlater": {
+                        this.coin = document.querySelector<any>(".u.coin");
+                        this.fav = document.querySelector<any>(".u.fav");
+                        return this.coin && this.fav && aid;
+                    }
+                    default: {
+                        this.coin = document.querySelector<any>("[report-id*=coin]");
+                        this.fav = document.querySelector<any>("[report-id*=collect]");
+                        return this.coin && this.fav && aid;
+                    }
+                }
             }, () => this.init())
         }
         init() {
@@ -66,13 +79,183 @@ namespace API {
         }
         /** 修补样式 */
         style() {
-            let style = `.ulike {cursor: pointer;}.ulike svg{vertical-align: middle;margin-right: 10px;}`;
+            let style = `
+            .ulike {cursor: pointer;}.ulike svg{vertical-align: middle;margin-right: 10px;}
+            @keyframes shake {
+                2% {
+                    transform: translate(0.5px, -0.5px) rotate(0.5deg);
+                }
+                4% {
+                    transform: translate(-0.5px, 2.5px) rotate(0.5deg);
+                }
+                6% {
+                    transform: translate(-1.5px, 2.5px) rotate(-0.5deg);
+                }
+                8% {
+                    transform: translate(-1.5px, 0.5px) rotate(1.5deg);
+                }
+                10% {
+                    transform: translate(1.5px, 2.5px) rotate(0.5deg);
+                }
+                12% {
+                    transform: translate(-1.5px, 2.5px) rotate(1.5deg);
+                }
+                14% {
+                    transform: translate(1.5px, 1.5px) rotate(-0.5deg);
+                }
+                16% {
+                    transform: translate(2.5px, -0.5px) rotate(1.5deg);
+                }
+                18% {
+                    transform: translate(1.5px, 0.5px) rotate(0.5deg);
+                }
+                20% {
+                    transform: translate(1.5px, -1.5px) rotate(-0.5deg);
+                }
+                22% {
+                    transform: translate(-1.5px, -1.5px) rotate(-0.5deg);
+                }
+                24% {
+                    transform: translate(-0.5px, -1.5px) rotate(0.5deg);
+                }
+                26% {
+                    transform: translate(-1.5px, 2.5px) rotate(-0.5deg);
+                }
+                28% {
+                    transform: translate(2.5px, 1.5px) rotate(1.5deg);
+                }
+                30% {
+                    transform: translate(0.5px, -0.5px) rotate(1.5deg);
+                }
+                32% {
+                    transform: translate(1.5px, 2.5px) rotate(1.5deg);
+                }
+                34% {
+                    transform: translate(-1.5px, -1.5px) rotate(-0.5deg);
+                }
+                36% {
+                    transform: translate(-0.5px, 0.5px) rotate(-0.5deg);
+                }
+                38% {
+                    transform: translate(1.5px, -1.5px) rotate(1.5deg);
+                }
+                40% {
+                    transform: translate(-0.5px, 0.5px) rotate(1.5deg);
+                }
+                42% {
+                    transform: translate(2.5px, -0.5px) rotate(1.5deg);
+                }
+                44% {
+                    transform: translate(-1.5px, 2.5px) rotate(0.5deg);
+                }
+                46% {
+                    transform: translate(-0.5px, 2.5px) rotate(-0.5deg);
+                }
+                48% {
+                    transform: translate(1.5px, -0.5px) rotate(1.5deg);
+                }
+                50% {
+                    transform: translate(1.5px, -0.5px) rotate(1.5deg);
+                }
+                52% {
+                    transform: translate(0.5px, 0.5px) rotate(-0.5deg);
+                }
+                54% {
+                    transform: translate(-0.5px, -1.5px) rotate(-0.5deg);
+                }
+                56% {
+                    transform: translate(1.5px, -1.5px) rotate(1.5deg);
+                }
+                58% {
+                    transform: translate(0.5px, 1.5px) rotate(1.5deg);
+                }
+                60% {
+                    transform: translate(-0.5px, -0.5px) rotate(1.5deg);
+                }
+                62% {
+                    transform: translate(-0.5px, 0.5px) rotate(1.5deg);
+                }
+                64% {
+                    transform: translate(0.5px, -0.5px) rotate(1.5deg);
+                }
+                66% {
+                    transform: translate(2.5px, 0.5px) rotate(1.5deg);
+                }
+                68% {
+                    transform: translate(1.5px, -0.5px) rotate(1.5deg);
+                }
+                70% {
+                    transform: translate(0.5px, 2.5px) rotate(1.5deg);
+                }
+                72% {
+                    transform: translate(1.5px, -0.5px) rotate(0.5deg);
+                }
+                74% {
+                    transform: translate(2.5px, -0.5px) rotate(0.5deg);
+                }
+                76% {
+                    transform: translate(2.5px, -0.5px) rotate(1.5deg);
+                }
+                78% {
+                    transform: translate(-1.5px, -0.5px) rotate(-0.5deg);
+                }
+                80% {
+                    transform: translate(-1.5px, 0.5px) rotate(-0.5deg);
+                }
+                82% {
+                    transform: translate(-1.5px, 2.5px) rotate(0.5deg);
+                }
+                84% {
+                    transform: translate(-0.5px, 0.5px) rotate(1.5deg);
+                }
+                86% {
+                    transform: translate(-1.5px, 1.5px) rotate(0.5deg);
+                }
+                88% {
+                    transform: translate(-0.5px, -0.5px) rotate(1.5deg);
+                }
+                90% {
+                    transform: translate(-1.5px, -1.5px) rotate(1.5deg);
+                }
+                92% {
+                    transform: translate(2.5px, 1.5px) rotate(0.5deg);
+                }
+                94% {
+                    transform: translate(2.5px, 1.5px) rotate(-0.5deg);
+                }
+                96% {
+                    transform: translate(2.5px, -0.5px) rotate(0.5deg);
+                }
+                98% {
+                    transform: translate(1.5px, 0.5px) rotate(-0.5deg);
+                }
+                0%,
+                  100% {
+                    transform: translate(0, 0) rotate(0);
+                }
+            }`;
             switch (this.type) {
-                case "bangumi": style += `.ulike {margin-left: 15px;position: relative;float: left;height: 100%;line-height: 18px;font-size: 12px;color: #222;}`;
+                case "bangumi": {
+                    style += `
+                    .bangumi-header .header-info .count-wrapper .ulike {margin-left: 15px;position: relative;float: left;height: 100%;line-height: 18px;font-size: 12px;color: #222;}
+                    .bangumi-header .header-info .count-wrapper [report-id*=coin].circle {}`;
                     break;
-                case "watchlater": style += `.video-info-module .number .ulike {margin-left: 15px;margin-right: 5px;}`;
+                }
+                case "watchlater": {
+                    style += `
+                    .video-info-module .number .ulike {margin-left: 15px;margin-right: 5px;}
+                    .video-info-module .number .ulike.shake svg {animation: shake 1500ms ease-in-out infinite;}
+                    .video-info-module .number .u.coin.circle {}
+                    .video-info-module .number .u.fav.circle {}`;
                     break;
-                default: style += `.video-info-m .number .ulike {margin-left: 15px;margin-right: 5px;}`;
+                }
+                default: {
+                    style += `
+                    .video-info-m .number .ulike {margin-left: 15px;margin-right: 5px;}
+                    .video-info-m .number .ulike.shake svg {animation: shake 1500ms ease-in-out infinite;}
+                    .video-info-m .number [report-id*=coin].circle {}
+                    .video-info-m .number [report-id*=collect].circle {}`;
+                }
             }
             addCss(style);
         }
